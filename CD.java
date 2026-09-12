20ff4a60b3b929aa90c8d4088b7fe2fd25e012f0
duckconan48-lab <duckconan48@gmail.com>
2026-09-12 20:29:57 +0700

    Add files via upload

---
+++ CD.java
@@ -0,0 +1,83 @@
+package Tuan3_Bai8;
+
+public class CD 
+{
+	private String maCD;      
+	private String tuaCD;     
+	private String caSy;    
+	private int soBaiHat;   
+	private float gia;
+	public String getMaCD() 
+	{
+		return maCD;
+	}
+	public void setMaCD(String maCD) 
+	{
+		this.maCD = maCD;
+	}
+	public String getTuaCD() 
+	{
+		return tuaCD;
+	}
+	public void setTuaCD(String tuaCD) 
+	{
+		this.tuaCD = tuaCD;
+	}
+	public String getCaSy() 
+	{
+		return caSy;
+	}
+	public void setCaSy(String caSy) 
+	{
+		this.caSy = caSy;
+	}
+	public int getSoBaiHat() 
+	{
+		return soBaiHat;
+	}
+	public void setSoBaiHat(int soBaiHat) throws Exception
+	{
+		if(soBaiHat>0)
+		{
+			this.soBaiHat = soBaiHat;
+		}
+		else
+		{
+			throw new Exception("So bai hat phai lon hon 0");
+		}
+	}
+	public float getGia() 
+	{
+		return gia;
+	}
+	public void setGia(float gia) 
+	{
+		this.gia = gia;
+	}
+	public CD() 
+	{
+	}
+	public CD(String maCD, String tuaCD, String caSy, int soBaiHat, float gia) throws Exception
+	{
+		this.maCD = maCD;
+		this.tuaCD = tuaCD;
+		this.caSy = caSy;
+		this.setSoBaiHat(soBaiHat);
+		this.gia = gia;
+	}
+	public static void tieuDe() 
+	{
+        System.out.println("In danh sach CD");
+        for (int i = 0; i < 100; i++) System.out.print("-");
+        System.out.println();
+        String s = String.format("|%10s|%10s|%10s|%10s|%10s|%10s|","Ma CD", "Tua CD", "Ca Sy","So Bai", "Don Gia", "Thanh Tien");
+        System.out.println(s);
+        for (int i = 0; i < 100; i++) System.out.print("-");
+        System.out.println();
+    }
+	@Override
+	public String toString() 
+	{
+		return String.format("|%10s|%10s|%10s|%10d|%10.2f|%10.2f|", maCD, tuaCD, caSy, soBaiHat, gia, gia * soBaiHat);
+	}
+}
